f8c99b7a972306cd2c2f3567ab836905eac08f6b
Leonardo <leonardo@odeliverypro.com>
2023-08-29 02:40:13 -0300

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Hash;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -22,7 +23,7 @@ class UserFactory extends Factory
             'is_admin' => false,
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('userpassword'),
             'avatar' => null,
             'address' => fake()->realText(255),
             'phone_number' => fake()->phoneNumber(),
@@ -48,6 +49,7 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'is_admin' => true,
+            'password' => Hash::make('admin')
         ]);
     }
 
