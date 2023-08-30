621cccdbc2db7211275b2e3e3a0e1b3cf79f8682
Leonardo <leonardo@odeliverypro.com>
2023-08-30 17:38:17 -0300

    Suppress Unused parameter $context.

--- app/Casts/HashPasswordCast.php
+++ app/Casts/HashPasswordCast.php
@@ -9,17 +9,19 @@ use App\Exceptions\Casts\InvalidTypeForPasswordCast;
 class HashPasswordCast implements Cast
 {
     /**
-     * @throws \Throwable
      * @param array<array-key> $context
-     * @inheritdoc
+     * @phpcsSuppress SlevomatCodingStandard.Functions.UnusedParameter
+     *
+     * @throws \Throwable
      */
     public function cast(DataProperty $property, mixed $value, array $context): mixed
     {
         throw_if(
-            !$property->type->acceptsType('string'),
+            ! $property->type->acceptsType('string'),
             InvalidTypeForPasswordCast::class,
             __('Only strings can be casted.')
         );
+
         return \Hash::make($value);
     }
 }
