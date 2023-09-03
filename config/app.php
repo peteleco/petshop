5c6db99606d0e470f5c820654ac9407f9c29515f
Leonardo <leonardo@odeliverypro.com>
2023-09-03 15:45:53 -0300

    add ide helper package
    https://github.com/barryvdh/laravel-ide-helper

--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\ContractServiceProvider::class
+        App\Providers\ContractServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ])->toArray(),
 
     /*
