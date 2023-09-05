47c6c3cf5e6defb04b2520d3cc905799d4989d7a
Leonardo <leonardo@odeliverypro.com>
2023-09-04 22:55:10 -0300

    add snap shop test and notifier package

---
+++ tests/ValueObjects/Payment/BankTransferTest.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\ValueObjects\Payment\BankTransfer;
+
+it('transform to json schema', function (){
+    // dd(BankTransfer::jsonSchema());
+    expect(BankTransfer::jsonSchema())->toMatchSnapshot();
+});
