7376753145f8495f759e206fa5ffb3914d1ecbcf
Leonardo <leonardo@odeliverypro.com>
2023-09-04 22:58:03 -0300

    create a PaymentDetail data
    jsonSchema transform the structure into json to be stored

--- app/ValueObjects/Payment/PaymentDetail.php
+++ app/ValueObjects/Payment/PaymentDetail.php
@@ -2,9 +2,30 @@
 
 namespace App\ValueObjects\Payment;
 
+use Carbon\Carbon;
+use ReflectionClass;
 use Spatie\LaravelData\Data;
+use Spatie\TypeScriptTransformer\Transformers\DtoTransformer;
+use Spatie\TypeScriptTransformer\TypeScriptTransformerConfig;
 
 abstract class PaymentDetail extends Data
 {
+    public static function jsonSchema():string
+    {
+        $reflection = new ReflectionClass(static::class);
+        /** @var array-key $schema*/
+        $schema = [];
+        foreach ($reflection->getConstructor()->getParameters() as $parameter) {
+            $schema[$parameter->getName()] = static::transformToStringType($parameter->getType()->getName());
+        }
 
+        return json_encode($schema);
+    }
+    private static function transformToStringType(string $type): string
+    {
+        if(in_array($type,[Carbon::class, \DateTimeInterface::class])) {
+            return 'string';
+        }
+        return $type;
+    }
 }
