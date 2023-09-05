b49e68ad3cea9942b44d8fef3400fae21ff69aea
Leonardo <leonardo@odeliverypro.com>
2023-09-05 11:24:48 -0300

    fix static analysis

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,11 +3,10 @@
 namespace App\Models;
 
 use App\Concerns\HasUuid;
-use Database\Seeders\OrderStatusSeeder;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Peteleco\Notifier\Events\OrderUpdated;
 use Peteleco\Notifier\OrderUpdatedMessage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
@@ -17,19 +16,22 @@ class Order extends Model
     {
         parent::boot();
         // Dispatch events
-        static::created(function (Order $order) {
+        static::created(function (Order $order): void {
+            /** @var string $status*/
+            $status = $order->load('orderStatus')->getRelation('orderStatus')->getAttribute('title');
             OrderUpdated::dispatch(OrderUpdatedMessage::from([
                 'uuid' => $order->getAttribute('uuid'),
-                'status' => $order->load('orderStatus')->orderStatus->getAttribute('title'),
+                'status' => $status,
                 'updated_at' => $order->getAttribute('created_at'),
             ]));
         });
 
-        static::updated(function (Order $order) {
-            if($order->wasChanged('order_status_id')) {
+        static::updated(function (Order $order): void {
+            if ($order->wasChanged('order_status_id')) {
+                $status = $order->load('orderStatus')->getRelation('orderStatus')->getAttribute('title');
                 OrderUpdated::dispatch(OrderUpdatedMessage::from([
                     'uuid' => $order->getAttribute('uuid'),
-                    'status' =>  $order->load('orderStatus')->orderStatus->getAttribute('title'),
+                    'status' => $status,
                     'updated_at' => $order->getAttribute('created_at'),
                 ]));
             }
@@ -37,7 +39,6 @@ class Order extends Model
     }
 
     /**
-     *
      * @var array<string, string> $casts
      * @inheritdoc
      */
@@ -46,15 +47,15 @@ class Order extends Model
     ];
 
     /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\OrderStatus>
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\OrderStatus,\App\Models\Order>
      */
     public function orderStatus(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
-        return $this->belongsTo(OrderStatus::class, 'order_status_id');
+        return $this->belongsTo(OrderStatus::class, 'order_status_id', 'id');
     }
 
     /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\Payment>
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\Payment, \App\Models\Order>
      */
     public function payment(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
